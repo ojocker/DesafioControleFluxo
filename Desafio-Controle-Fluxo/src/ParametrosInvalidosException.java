a70cfcea4ba4effc1fe54bcb9ff0e689ba851811
ojocker <alex.bueno.santiago@hotmail.com>
2025-03-30 13:40:30 -0300

    Add files via upload

---
+++ Desafio-Controle-Fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+
+// Arquivo ParametrosInvalidosException.java
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
